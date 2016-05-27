121e3432e18210de2768fe0fde9042c864cfd2d9
Pedro <pedroob221@gmail.com>
2016-05-27 23:05:41 +0100

    Added fancybox for image zooming

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
 
 $(document).ready(function(){/* activate sidebar */
-$('#sidebar').affix({
-  offset: {
-    top: 235
-  }
-});
+  $('#sidebar').affix({
+    offset: {
+      top: 235
+    }
+  });
 
-/* activate scrollspy menu */
-var $body   = $(document.body);
-var navHeight = $('.navbar').outerHeight(true) + 10;
+  /* activate scrollspy menu */
+  var $body   = $(document.body);
+  var navHeight = $('.navbar').outerHeight(true) + 10;
 
-$body.scrollspy({
-	target: '#leftCol',
-	offset: navHeight
-});
+  $body.scrollspy({
+    target: '#leftCol',
+    offset: navHeight
+  });
 
-/* smooth scrolling sections */
-$('a[href*=#]:not([href=#])').click(function() {
+  /* smooth scrolling sections */
+  $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -27,7 +27,15 @@ $('a[href*=#]:not([href=#])').click(function() {
         return false;
       }
     }
-});
+  });
+
+  $(".fancybox").fancybox({
+    helpers: {
+      overlay: {
+        locked: false
+      }
+    }
+  });
 });
 
 $('.tree-toggle').click(function () {
